40c53866c45043156e84a5510b1d45e510f68040
GrelaR <rodrigogrela@hotmail.com>
2022-09-25 21:36:41 -0300

    Item interface in adapter created

--- app/src/main/java/com/example/bankuishchallenge/mainscreen/presentation/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/bankuishchallenge/mainscreen/presentation/adapter/RecyclerAdapter.kt
@@ -2,6 +2,7 @@ package com.example.bankuishchallenge.mainscreen.presentation.adapter
 
 import android.content.Context
 import android.view.LayoutInflater.from
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -9,22 +10,30 @@ import com.example.bankuishchallenge.core.BaseViewHolder
 import com.example.bankuishchallenge.databinding.RepositorieItemBinding
 import com.example.bankuishchallenge.mainscreen.data.model.RepositoriesItemModel
 
-class RecyclerAdapter(private val context: Context, private val listRepos: List<RepositoriesItemModel>): RecyclerView.Adapter<BaseViewHolder<*>>() {
+class RecyclerAdapter(
+    private val context: Context,
+    private val listRepos: List<RepositoriesItemModel>,
+    private val itemClickListener: OnRepoClickListener
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         return ReposViewHolder(RepositorieItemBinding.inflate(from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        when(holder) {
-            is ReposViewHolder -> holder.bind(listRepos[position],position)
+        when (holder) {
+            is ReposViewHolder -> holder.bind(listRepos[position], position)
         }
     }
 
     override fun getItemCount(): Int = listRepos.size
 
-    inner class ReposViewHolder(private val binding: RepositorieItemBinding): BaseViewHolder<RepositoriesItemModel>(binding.root) {
+    inner class ReposViewHolder(private val binding: RepositorieItemBinding) :
+        BaseViewHolder<RepositoriesItemModel>(binding.root) {
         override fun bind(item: RepositoriesItemModel, position: Int) {
             with(binding) {
+                cardView.setOnClickListener {
+                    itemClickListener.onRepoClick(it)
+                }
                 tvAuthorName.text = item.name
                 tvRepoName.text = item.full_name
                 Glide.with(context)
@@ -32,5 +41,9 @@ class RecyclerAdapter(private val context: Context, private val listRepos: List<
                     .into(ivRepoOwner)
             }
         }
+    }
+
+    interface OnRepoClickListener {
+        fun onRepoClick(view: View)
     }
 }
